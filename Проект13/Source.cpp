95b1eb1d585a3f1e770c16832bbd8d00340b9375
lenovo <lenovo@LENOVO->
2018-06-08 22:48:08 +0600

    -

--- Проект13/Source.cpp
+++ Проект13/Source.cpp
@@ -6,8 +6,29 @@ int main() {
 	setlocale(LC_ALL, "russian");
 	srand(time( NULL));
 
-	vector<queue<Car>> boxes;
-	//boxes.
+	vector<queue<Car>> boxes(3);
+	
+	queue <Car> box1, box2, box3;
+
+	Car first("lexus", 15);
+	Car second("suzuki", 30);
+	Car third("volvo", 20); 
+	
+
+	box1.push(first);
+	box2.push(second);
+	box3.push(third);
+
+	boxes.push_back(box1);
+	boxes.push_back(box2);
+	boxes.push_back(box2);
+	
+
+	/*for (int i = 0; i < 3; i++)
+		cout <<boxes.at(i) << endl;
+	*/
+	for (auto it = boxes.begin(); it<boxes.end(); ++it)
+		cout << *it << ends;
 
 	system("pause");
 	return 0;
